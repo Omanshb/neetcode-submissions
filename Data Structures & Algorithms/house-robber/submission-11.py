49ac1ec86672aadc2eff6c5feb044e34bbd96b26
Omansh Bainsla <73659149+Omanshb@users.noreply.github.com>
2026-04-22 14:14:12 -0700

    Add: house-robber - submission-11

---
+++ Data Structures & Algorithms/house-robber/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+
+        def dp(i):
+            if i >= len(nums):
+                return 0
+            
+            if memo[i] != -1:
+                return memo[i]
+            
+            skip = dp(i + 1)
+            rob = dp(i + 2) + nums[i]
+
+            memo[i] = max(skip, rob)
+            return memo[i]
+        
+        return dp(0)
