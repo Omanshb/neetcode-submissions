55c414e2bf0b97ec986f93a1aa41825ed893ab26
Omansh Bainsla <73659149+Omanshb@users.noreply.github.com>
2026-04-22 15:14:39 -0700

    Add: decode-ways - submission-14

---
+++ Data Structures & Algorithms/decode-ways/submission-14.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = [-1] * len(s)
+
+        def dp(i):
+            if i >= len(s):
+                return 1
+
+            if s[i] == '0':
+                return 0
+            
+            if memo[i] != -1:
+                return memo[i]
+            
+            ways = 0
+            
+            if i + 1 in range(len(s)):
+                if int(s[i: i + 2]) < 27:
+                    ways = dp(i + 2) + dp(i + 1)
+                else:
+                    ways = dp(i + 1)
+            else:
+                ways = dp(i + 1)
+            
+            memo[i] = ways
+            return memo[i]
+        
+        return dp(0)
