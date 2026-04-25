b43973951b38c2cae68f6b8b0d521236e7a2cf86
Omansh Bainsla <73659149+Omanshb@users.noreply.github.com>
2026-04-25 15:15:22 -0700

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        memo = {}
+
+        def dp(i, left):
+            if left <= 0 or i >= len(coins):
+                return int(left == 0)
+            
+            if (i, left) in memo:
+                return memo[(i, left)]
+            
+            memo[(i, left)] = dp(i + 1, left) + dp(i, left - coins[i])
+            return memo[(i, left)]
+        
+        return dp(0, amount)
